28db80c3ad32ea408ce2d0b4f21374791b60bc3e
saanvitel <saanvi.telukoti@gmail.com>
2023-04-28 14:39:11 +1000

    Displays the temperature and location of a place, that is called from an API from another website. Can also swap from celcius and fahrenheit.

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,34 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import React, { useState, useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [temp, setTemp] = useState(20);
+  const [isCelcius, setIsCelcius] = useState(true);
+  const [address, setAddress] = useState("a place near you");
+
+  // Fetch the weather only once
+  useEffect(()=>{
+    fetchWeather();
+  },[]);
+
+  // API call to another website, which takes some time to return a result
+  async function fetchWeather() {
+    const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/melbourne?unitGroup=metric&key=F34RGXAKG75EX69MUH7E6W7WQ&contentType=json", {
+      "method": "GET",
+      "headers": {
+      }
+      })
+    // Here the weather data gets returned
+    const weather = await response.json();
+    setTemp(weather.currentConditions.temp);
+    setAddress(weather.address);
+  }
+
+
   return (
     <>
       <Head>
@@ -17,98 +41,13 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
+            The temperature is:&nbsp;
+            <code className={styles.code}>{isCelcius ? temp : Math.floor((temp*9/5) + 32)}</code> &nbsp;
+             {isCelcius ? "celcius" : "fahrenheit"} in&nbsp;
+            <code className={styles.code}>{address}</code>
           </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+          <button onClick={()=>{setIsCelcius(!isCelcius)}}>Hello</button>
+          </div></main> 
     </>
   )
 }
